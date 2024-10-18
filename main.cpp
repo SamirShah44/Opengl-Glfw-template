94e7210b742d0fe2ae6d7d510ac0ef097fd6032d
SamirShah44 <shahsamir4444@gmail.com>
2024-10-18 18:46:23 -0500

    Draw two triagnle side by side with different color (yellow and orange)

--- main.cpp
+++ main.cpp
@@ -17,21 +17,25 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "{\n"
                                    "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
                                    "}\n\0";
-
+const char *fragmentShader2Source = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+    "}\n\0";
 int main()
 {
     glfwInit();
 
+    // Tell GLFW what version of OpenGL we are using
+    // In this case we are using OpenGL 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Tell GLFW we are using the CORE profile
+    // So that means we only have the modern functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLfloat vertices[] = {
-        -0.5f, 0.5f, -0.5f,
-        0.3f, 0.4f, 0.6f,
-        0.8f, 0.9f, -0.5f
-        };
-
+    // Create a GLFWwindow object of 800 by 800 pixels, naming it "FIrst try"
     GLFWwindow *window = glfwCreateWindow(900, 800, "FIrst try", NULL, NULL);
     if (window == NULL)
     {
@@ -41,44 +45,113 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
+    // Load GLAD so it configures OpenGL
+    gladLoadGL();
+    // Specify the viewport of OpenGL in the Window
+    // In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+    glViewport(0, 0, 800, 800);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    GLfloat vertices[] = {
+         0.2f,  0.5f, 0.0f,  // top right
+         0.2f, -0.2f, 0.0f,  // bottom right
+        -0.2f, -0.2f, 0.0f,  // bottom left
+        -0.2f,  0.8f, 0.0f,  // top left 
+        0.5f,  0.5f, 0.0f,
+        0.3f, -0.3f, 0.0f,
+    };
+    GLfloat vertices1[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.3f, -0.3f, 0.0f,  // bottom right
+        -0.2f, -0.3f, 0.0f,  // bottom left
+        -0.2f,  0.3f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 3, 1,  // first Triangle
+        0, 3, 1   // second Triangle
+    };
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+    unsigned int shaderProgramYellow = glCreateProgram();
+    unsigned int fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
-
+    glShaderSource(fragmentShaderYellow, 1, &fragmentShader2Source, NULL);
+    glCompileShader(fragmentShaderYellow);
     GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram , vertexShader);
-    glAttachShader(shaderProgram , fragmentShader);
-
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+
+
+    glAttachShader(shaderProgramYellow, vertexShader);
+    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
+    glLinkProgram(shaderProgramYellow);
+
+    glDeleteShader(fragmentShaderYellow);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    GLuint VAO, VBO, EBO;
+    glGenBuffers(1, &EBO);
 
+    // Generate the VAO and VBO with only 1 object each
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    GLuint VAO,VBO;
-    glGenVertexArrays(1,&VAO);
-    glGenBuffers(1,&VBO);
-
+    // Make the VAO the current Vertex Array Object by binding it
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices,GL_STATIC_DRAW);
+    // Bind the VBO specifying it's a GL_ARRAY_BUFFER
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Introduce the vertices into the VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    gladLoadGL();
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-    glViewport(0, 0, 900, 800);
+    // Similar to VBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // Enable the Vertex Attribute so that OpenGL knows to use it
+    glEnableVertexAttribArray(0);
+
+    // Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 1, 3);
+        glUseProgram(shaderProgramYellow);
+        glDrawArrays(GL_TRIANGLES, 3,3);
+
+        //Replacing this with another one to draw multiple one
+// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+glBindVertexArray(0);
+        glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+glDeleteProgram(shaderProgramYellow);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
